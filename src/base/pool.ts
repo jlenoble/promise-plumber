c7813b76c0ba90ae016adcc42dd835fcba0dc7a1
Jason Lenoble <jason.lenoble@gmail.com>
2019-06-07 13:47:50 +0200

    Reject on first error

--- src/base/pool.ts
+++ src/base/pool.ts
@@ -1,56 +1,61 @@
-import { Executor, explicitResolutionExecutor } from "./executors";
+import { Executor, explicitDecisionExecutor } from "./executors";
 import { ResolvableState } from "./states";
 
 import { AwaitSafeResolutionPromise } from "./triggers";
 
 export class Pool<T> extends AwaitSafeResolutionPromise<T[]> {
-  protected readonly _elements: Set<T | PromiseLike<T>>;
-  protected readonly _resolvedElements: T[];
+  protected _nPendingValues: number;
+  protected readonly _resolvedValues: T[];
 
   public constructor(executor?: Executor<T[]>) {
-    super(executor || new ResolvableState(), explicitResolutionExecutor);
-    this._elements = new Set();
-    this._resolvedElements = [];
+    super(executor || new ResolvableState(), explicitDecisionExecutor);
 
-    Object.defineProperty(this, "_elements", {
+    this._nPendingValues = 0;
+    this._resolvedValues = [];
+
+    Object.defineProperty(this, "_nPendingValues", {
+      writable: true,
+      enumerable: false,
+      configurable: false
+    });
+
+    Object.defineProperty(this, "_resolvedValues", {
       writable: false,
       enumerable: false,
       configurable: false
     });
 
-    Object.defineProperty(this, "_resolvedElements", {
-      writable: false,
-      enumerable: false,
-      configurable: false
-    });
-
-    Object.freeze(this);
+    Object.seal(this);
   }
 
-  public add(element: T | PromiseLike<T>): boolean {
-    if (this._elements.has(element) || this._state.done) {
-      return false;
-    }
+  public add(value: T | PromiseLike<T>): this {
+    if (!this._state.done) {
+      this._nPendingValues++;
 
-    this._elements.add(element);
+      Promise.resolve(value).then(
+        (val: T): void => {
+          if (!this._state.done) {
+            this._resolvedValues.push(val);
+            this._nPendingValues--;
 
-    Promise.resolve(element)
-      .then(
-        (el: T): void => {
-          this._resolvedElements.push(el);
+            if (this._nPendingValues === 0) {
+              Object.freeze(this._resolvedValues);
+              this._state.resolve(this._resolvedValues);
+              Object.freeze(this);
+            }
+          }
         },
-        (): void => {}
-      )
-      .finally(
-        (): void => {
-          this._elements.delete(element);
-
-          if (this._elements.size === 0) {
-            this._state.resolve(this._resolvedElements);
+        (err: Error): void => {
+          if (!this._state.done) {
+            this._nPendingValues = 0;
+            this._resolvedValues.length = 0;
+            this._state.reject(err);
+            Object.freeze(this);
           }
         }
       );
+    }
 
-    return true;
+    return this;
   }
 }
